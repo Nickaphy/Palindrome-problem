947e3ab99678039fafc3496cbdc206eaf76a30a1
Nickaphy <nickdahchris@gmail.com>
2025-01-15 13:46:09 +0100

    Palindrome problem

---
+++ Palindrome.cs
@@ -0,0 +1,24 @@
+﻿class program
+{
+    static void Main()
+    {
+        bool ispalindrome = palindrome();
+        Console.WriteLine(ispalindrome);
+    }
+
+    public static bool palindrome()
+    {
+        int x = 343; //Integeren vi tester
+        int reversed = 0; //Deklarere vores reversed værdi
+        int original = x; //Gemmer x's orginale værdi
+
+        while(x != 0) //mens x ikke er 0 
+        {
+            int lastdigit = x % 10; //bruger modulus til at finde de sidste tal i x
+            reversed = reversed * 10 + lastdigit; 
+            //adder tallet til reversed og skubber det mod venstre ved at gange med 10
+            x = x/10; //fjerner tallet fra x ved at dividere med 10
+        }
+        return original == reversed; //returnere true eller false efter udsagnet
+    }
+}
