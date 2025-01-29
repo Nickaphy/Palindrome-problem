2c76c71add68eeaf7efa3f12a7c5544b8371f8cd
Nicklas Christoffersen <93937135+Nickaphy@users.noreply.github.com>
2025-01-29 19:30:25 +0100

    Update Palindrome.cs
    Fixed solution to take in account for negative numbers.

--- Palindrome.cs
+++ Palindrome.cs
@@ -1,24 +1,14 @@
-﻿class program
-{
-    static void Main()
-    {
-        bool ispalindrome = palindrome();
-        Console.WriteLine(ispalindrome);
-    }
+public class Solution {
+    public bool IsPalindrome(int x) {
+        int original = x;
+        int reversed = 0;
 
-    public static bool palindrome()
-    {
-        int x = 343; //Integeren vi tester
-        int reversed = 0; //Deklarere vores reversed værdi
-        int original = x; //Gemmer x's orginale værdi
-
-        while(x != 0) //mens x ikke er 0 
+        while (x != 0 & x > 0)
         {
-            int lastdigit = x % 10; //bruger modulus til at finde de sidste tal i x
-            reversed = reversed * 10 + lastdigit; 
-            //adder tallet til reversed og skubber det mod venstre ved at gange med 10
-            x = x/10; //fjerner tallet fra x ved at dividere med 10
+            int lastdigit = x % 10;
+            reversed = reversed * 10 + lastdigit;
+            x = x/10;
         }
-        return original == reversed; //returnere true eller false efter udsagnet
+        return reversed == original;
     }
 }
